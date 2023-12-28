f820bc1746765509a656356ef6f35417a9a588d1
Anoop Lamba <anooplamba77@gmail.com>
2023-12-28 20:32:23 +0530

    added .env

--- src/FireBase.js
+++ src/FireBase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBNNCBaxWnFulcMKZ7joeTddEMzf6nDjm8",
-  authDomain: "photofolio-dce8b.firebaseapp.com",
-  projectId: "photofolio-dce8b",
-  storageBucket: "photofolio-dce8b.appspot.com",
-  messagingSenderId: "275043433167",
-  appId: "1:275043433167:web:c4b44b79d1d75143c715ba",
+  apiKey: process.env.REACT_APP_ApiKey,
+  authDomain: process.env.REACT_APP_AuthDomain,
+  projectId: process.env.REACT_APP_ProjectId,
+  storageBucket: process.env.REACT_APP_StorageBucket,
+  messagingSenderId: process.env.REACT_APP_MessagingSenderId,
+  appId: process.env.REACT_APP_AppId,
 };
 
 const app = initializeApp(firebaseConfig);
